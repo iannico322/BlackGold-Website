d26c6ddd71f7872e3957e2cea7e35f063f1796cf
iannico322 <73187566+iannico322@users.noreply.github.com>
2022-10-16 23:59:06 +0800

    asdsd

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,26 +1,23 @@
-const down = document.querySelector('.scroll')
-const last = document.querySelector('.last')
+const down = document.querySelector(".scroll");
+const last = document.querySelector(".last");
 
-const text = document.querySelector('.text')
-const arr = document.querySelector('.arr')
-let direction = "#last"
-let di = 2
-down.addEventListener('click',()=>{
-    if(di  % 2 ==0){
-        direction =  "#last"
-        arr.textContent = " "
-        text.textContent = " Scroll Up  •  >"
-        
-    }else{
-        direction =  "#main"
-        arr.textContent = "< • "
-        text.textContent = "Scroll Down "
-    }
-    last.href = direction;
-    
-    last.click()
-    console.log(di)
-    di += 1;
-    
+const text = document.querySelector(".text");
+const arr = document.querySelector(".arr");
+let direction = "#last";
+let di = 2;
+down.addEventListener("click", () => {
+  if (di % 2 == 0) {
+    direction = "#last";
+    arr.textContent = " ";
+    text.textContent = " Scroll Up  •  >";
+  } else {
+    direction = "#main";
+    arr.textContent = "< • ";
+    text.textContent = "Scroll Down ";
+  }
+  last.href = direction;
 
-})
+  last.click();
+  console.log(di);
+  di += 1;
+});
