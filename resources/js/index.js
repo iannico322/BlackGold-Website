c48e9d285404923425671721c2d65153db0c5006
iannico322 <73187566+iannico322@users.noreply.github.com>
2022-10-17 23:22:18 +0800

    final collapse navbar

--- resources/js/index.js
+++ resources/js/index.js
@@ -26,6 +26,19 @@ down.addEventListener("click", () => {
   di += 1;
 });
 
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    options.style.visibility = "hidden"
+    
+  } else {
+    options.style.visibility = "visible"
+    icon.className = "fa fa-bars hide"
+  }
+}
+
+var x = window.matchMedia("(max-width: 1200px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
 let btn_counter = 2
 borger.addEventListener('click',()=>{
   if(btn_counter % 2 ==0){
